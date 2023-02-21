270ca1ac036d208992dd65df02fc4bd6ea2d8dba
marcodenic <36410465+marcodenic@users.noreply.github.com>
2023-02-21 02:41:44 +0000

    Fix header url and coin page alignment

--- components/Layout.js
+++ components/Layout.js
@@ -11,21 +11,13 @@ const Layout = ({ children, title = "Crypto Dash" }) => {
         <Script src="node_modules\flowbite\dist\flowbite.min.js" />
       </Head>
       <header className="header dark">
-        {/* <Link href="/" passHref>
-          <div className="logo bg-slate-300">{title}</div>
-        </Link> */}
         <nav class="bg-white border-gray-200 px-4 lg:px-6 w-full py-2.5 dark:bg-gray-800">
           <div class="flex flex-wrap  max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-              {/* <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                class="mr-3 h-6 sm:h-9"
-                alt="Flowbite Logo"
-              /> */}
+            <Link href="/" passHref class="flex items-center">
               <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 {title}
               </span>
-            </a>
+            </Link>
           </div>
         </nav>
       </header>
